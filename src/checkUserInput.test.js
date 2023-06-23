67f8c35962ee25ec726c653f0a96e13fd89f8bd6
heonq <gkagjsrb@naver.com>
2023-06-23 14:14:25 +0900

    test: 입력한 값에 중복된 숫자가 있을 경우 에러 발생하는 테스트 코드 작성 -ERROR_MESSAGE.DUPLICATED 메세지가 정상적으로 표시되는지 확인한다.

--- src/checkUserInput.test.js
+++ src/checkUserInput.test.js
@@ -9,4 +9,12 @@ describe("사용자의 입력값이 유효한지 테스트한다.",()=>{
             return checkUserInput(answer);
         })).toThrow(Error(ERROR_MESSAGE.THREE_DIGITS));
     })
+
+    test("중복된 숫자가 있을 경우 에러 발생",()=>{
+        const answers=["112","211","131"];
+
+        expect(()=>answers.forEach((answer)=>{
+            return checkUserInput(answer);
+        })).toThrow(Error(ERROR_MESSAGE.DUPLICATED));
+    })
 })
