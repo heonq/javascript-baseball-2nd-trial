334944d646d0de40370ed9ac31bc61d7c14af425
heonq <gkagjsrb@naver.com>
2023-06-23 14:11:08 +0900

    test: 3자리의 숫자를 입력하지 않을 경우 에러가 발생하는 테스트 코드 작성 -에러 메세지가 정상적으로 출력되는지 확인

---
+++ src/checkUserInput.test.js
@@ -0,0 +1,12 @@
+const checkUserInput = require("./checkUserInput.js");
+const {ERROR_MESSAGE} = require("./constants.js");
+
+describe("사용자의 입력값이 유효한지 테스트한다.",()=>{
+    test("3자리의 숫자를 입력하지 않을 경우 에러 발생",()=>{
+        const answers=["1","12","1546"];
+
+        expect(()=>answers.forEach((answer)=>{
+            return checkUserInput(answer);
+        })).toThrow(Error(ERROR_MESSAGE.THREE_DIGITS));
+    })
+})
